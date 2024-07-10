9ac236ad3ad36b18ed66d56be2034b91deca24fb
AuGrY <fduysakk@gmail.com>
2024-07-10 18:04:52 +0200

    Update project

---
+++ src/test/java/com/fleet_project/runners/CukesRunner.java
@@ -0,0 +1,20 @@
+package com.fleet_project.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/fleet_project/step_definitions",
+        dryRun = true,
+        tags = "@wip",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
